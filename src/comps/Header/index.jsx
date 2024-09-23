6ed654c60b997d23da360d3f9da5ab1079cd40b4
B3rK-3 <163774796+B3rK-3@users.noreply.github.com>
2024-09-23 14:58:20 -0400

    fixed header background

--- src/comps/Header/index.jsx
+++ src/comps/Header/index.jsx
@@ -10,6 +10,7 @@ import { FaDiscord } from 'react-icons/fa'
 
 
 const Header = () => {
+    //initialize variables
     const [sticked, setSticked] = useState(false);
     const navbarRef = useRef(null);
     const burgerRef = useRef(null);
@@ -19,7 +20,7 @@ const Header = () => {
     var x = 0;
     const widthElement = document.getElementById("width");
 
-
+    //functions needed for scrolling and menus
     const changeStickState = () => {
         if (window.scrollY > 0) {
             setSticked(true);
@@ -84,64 +85,63 @@ const Header = () => {
             <Bg />
             <div className=''>
                 <div>
-                <nav className={`${sticked ? 'sticked backdrop-blur-[3px] mt-[-40px]' : 'z-yes mb-[-120px] mt-[-30px]'} h-[150px]`}>
-                    <div className={`max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 z-0`}>
-                        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse my-2">
-                            <img src={banner} className="h-[100px]" alt="Boost Hacks Logo" />
-                        </a>
-                        <div className={`mt-[11px]`}>
-                            <button data-collapse-toggle="navbar-default" ref={burgerRef} type="button" className={`${toggled ? "hambr" : ''} flex flex-col justify-center transition-all text-sm text-gray-500 rounded-lg h-10 w-10 hover:bg-gray-100 hover:bg-gray-700`} aria-controls="navbar-default" aria-expanded={sticked ? "true" : "true"} id="burger" onClick={toggleMenu}>
-                                <span className="bar mb-1"></span>
-                                <span className="bar"></span>
-                                <span className="bar mt-[4px]"></span>
-                            </button>
-                        </div>
-                        <div className="hidden md:block w-auto" id="navbar-default" ref={navbarRef}>
-                            <ul ref={menu} className="gap-x-[-1px] mx-0 sm2:gap-x-[16.5px] font-medium flex flex-col p-3 pt-0 pb-1 md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 mt-3 mb-[7px] ">
-                                <li className="flex ">
-                                    <a href="#" className="under block py-2 px-2 text-white no-underline font-bold md2:text-lg" aria-current="page">Home</a>
-                                </li>
-                                <li className="flex">
-                                    <a href="#recap" className="under block py-2 px-2 text-white no-underline font-bold md2:text-lg">Past Hacks</a>
-                                </li>
-                                <li className="flex">
-                                    <a href="#sponsors" className="under block py-2 px-2 text-white no-underline font-bold md2:text-lg">Sponsors</a>
-                                </li>
-                                <li className="flex">
-                                    <a href="https://boost-hacks-ii.devpost.com/#judges" target="_blank" className="under block py-2 px-2 text-white no-underline font-bold md2:text-lg">Judges</a>
-                                </li>
-                                <li className="flex">
-                                    <a href="https://boost-hacks-ii.devpost.com/" target="_blank" className="under block py-2 px-2 text-white no-underline font-bold md2:text-lg mr-[-10px]">Hack In!</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-
-                <div className={`body mx-auto absolute z-50 mt-[50px]`}>
-                    <p className='block mt-20 p-0 align-middle text-center'>
-                        <span className='mb-[-16px] text-white text-[50px] roboto-slab-100 flex justify-center'>Boost Hacks II</span>
-                        <img className="h-[360px] sm:h-[400px] mx-auto p-0" src={logo} alt="Logo" />
-                    </p>
-                    <span id='yes' className="absolute boost-text top-[495px] sm:top-[540px] mb-0 bg-blue-400 py-0 px-0 xxxs:px-[0.2px] sm:p-2 w-[315px] sm:w-[315px] chip">
-                        <span className='pl-[10px] py-[2px] xxxs:ml-[2px] xxxs:px-[3px] sm:py-[3px] sm:px-[3.5px]'>June 17th - 25th, 2024</span>
-                        <span className='p-0 xxxs:px-[0.1px] sm:p-1 ml-[0px] mr-1'>Online Hackathon</span>
-                    </span>
-                    <div className="absolute flex items-center justify-center top-[547px] sm:top-[590px] w-full">
-                        <div className="container flex flex-col items-center justify-center text-white items-center align-middle w-100">
-                            <div className="row flex justify-center text-center w-full h-12 gap-x-4 gap-y-3 xs:gap-y-0 max-w-[315px] xs:max-w-[800px] mt-2">
-                                <span className="hRaise col-sm bg-blue-500 rounded-[12px] hover:cursor-pointer content-center py-2 text-[17px] flex justify-center items-center"><a href='https://boost-hacks-ii.devpost.com/' target='_blank' className='format flex hover:cursor-pointer'>Register On Devpost</a></span>
-                                <span ref={widthRef} className="hRaise col-sm bg-blue-500 rounded-[12px] content-center py-2 flex items-center justify-center text-[17px]">
-                                    <a href='https://discord.gg/FpgReGv4qp' target='_blank' className='format flex hover:cursor-pointer'>
-                                    <span>Join Our</span>
-                                    <FaDiscord id='width' color='white' className="ml-[4px] mt-[6px] spin" /></a>
-                                </span>
-                                <span className="hRaise col-sm bg-blue-500 rounded-[12px] content-center py-2 text-[17px] flex justify-center items-center"><a href='https://7y7w1tjdbbj.typeform.com/to/xgCXZ65q?typeform-source=boost-hacks-ii.devpost.com' target='_blank' className='format flex hover:cursor-pointer'>Be a Judge</a></span>
+                    <nav className={`h-[100px] w-full ${sticked ? 'sticked' : 'absolute z-100'} `}>
+                        <div className={`max-w-screen-xl flex flex-wrap items-center justify-between mx-auto mt-[-8px] px-[15px]`}>
+                            <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse my-2">
+                                <img src={banner} className="h-[100px]" alt="Boost Hacks Logo" />
+                            </a>
+                            <div className={`mt-[11px]`}>
+                                <button data-collapse-toggle="navbar-default" ref={burgerRef} type="button" className={`${toggled ? "hambr" : ''} flex flex-col justify-center transition-all text-sm text-gray-500 rounded-lg h-10 w-10 hover:bg-gray-100 hover:bg-gray-700`} aria-controls="navbar-default" aria-expanded={sticked ? "true" : "true"} id="burger" onClick={toggleMenu}>
+                                    <span className="bar mb-1"></span>
+                                    <span className="bar"></span>
+                                    <span className="bar mt-[4px]"></span>
+                                </button>
+                            </div>
+                            <div className="hidden md:block w-auto" id="navbar-default" ref={navbarRef}>
+                                <ul ref={menu} className="gap-x-[-1px] mx-0 sm2:gap-x-[16.5px] font-medium flex flex-col p-3 pt-0 pb-1 md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 mt-3 mb-[7px] ">
+                                    <li className="flex ">
+                                        <a href="#" className="under block py-2 px-2 text-white no-underline font-bold md2:text-lg" aria-current="page">Home</a>
+                                    </li>
+                                    <li className="flex">
+                                        <a href="#recap" className="under block py-2 px-2 text-white no-underline font-bold md2:text-lg">Past Hacks</a>
+                                    </li>
+                                    <li className="flex">
+                                        <a href="#sponsors" className="under block py-2 px-2 text-white no-underline font-bold md2:text-lg">Sponsors</a>
+                                    </li>
+                                    <li className="flex">
+                                        <a href="https://boost-hacks-ii.devpost.com/#judges" target="_blank" className="under block py-2 px-2 text-white no-underline font-bold md2:text-lg">Judges</a>
+                                    </li>
+                                    <li className="flex">
+                                        <a href="https://boost-hacks-ii.devpost.com/" target="_blank" className="under block py-2 px-2 text-white no-underline font-bold md2:text-lg mr-[-10px]">Hack In!</a>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
-                    </div>
+                    </nav>
+                    <div className={`absolute body mx-auto mt-12`}>
+                        <p className='block mt-20 p-0 align-middle text-center'>
+                            <span className='mb-[-16px] text-white text-[50px] roboto-slab-100 flex justify-center'>Boost Hacks II</span>
+                            <img className="h-[360px] sm:h-[400px] mx-auto p-0" src={logo} alt="Logo" />
+                        </p>
+                        <span className="absolute boost-text top-[495px] sm:top-[540px] mb-0 bg-blue-400 py-0 px-0 xxxs:px-[0.2px] sm:p-2 w-[315px] sm:w-[315px] chip">
+                            <span className='pl-[10px] py-[2px] xxxs:ml-[2px] xxxs:px-[3px] sm:py-[3px] sm:px-[3.5px]'>June 17th - 25th, 2024</span>
+                            <span className='p-0 xxxs:px-[0.1px] sm:p-1 ml-[0px] mr-1'>Online Hackathon</span>
+                        </span>
+                        <div className="absolute flex items-center justify-center top-[547px] sm:top-[590px] w-full">
+                            <div className="container flex flex-col items-center justify-center text-white items-center align-middle w-100">
+                                <div className="row flex justify-center text-center w-full h-12 gap-x-4 gap-y-3 xs:gap-y-0 max-w-[315px] xs:max-w-[800px] mt-2">
+                                    <span className="hRaise col-sm bg-blue-500 rounded-[12px] hover:cursor-pointer content-center py-2 text-[17px] flex justify-center items-center"><a href='https://boost-hacks-ii.devpost.com/' target='_blank' className='format flex hover:cursor-pointer'>Register On Devpost</a></span>
+                                    <span ref={widthRef} className="hRaise col-sm bg-blue-500 rounded-[12px] content-center py-2 flex items-center justify-center text-[17px]">
+                                        <a href='https://discord.gg/FpgReGv4qp' target='_blank' className='format flex hover:cursor-pointer'>
+                                            <span>Join Our</span>
+                                            <FaDiscord id='width' color='white' className="ml-[4px] mt-[6px] spin" /></a>
+                                    </span>
+                                    <span className="hRaise col-sm bg-blue-500 rounded-[12px] content-center py-2 text-[17px] flex justify-center items-center"><a href='https://7y7w1tjdbbj.typeform.com/to/xgCXZ65q?typeform-source=boost-hacks-ii.devpost.com' target='_blank' className='format flex hover:cursor-pointer'>Be a Judge</a></span>
+                                </div>
+                            </div>
+                        </div>
 
-                </div>
+                    </div>
                 </div>
             </div>
         </>
